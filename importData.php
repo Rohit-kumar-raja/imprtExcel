ceb51a912449be274fc9ba4c6766ec9065e48fde
rohit <rohit@rohit.com>
2021-08-29 23:24:41 +0530

    all done

--- importData.php
+++ importData.php
@@ -1,8 +1,32 @@
 <?php
 if(isset($_POST['submit'])){
-$doc_name=$_FILES;
-echo "<pre>";
-print_r($doc_name);
+
+$extention=$_FILES['doc']['name'];
+$extention=(pathinfo($extention))['extension'];
+if($extention=='xl' || $extention=="xls" || $extention=="xlsx" || $extention=="csv"){
+    require('./PHPExcel/PHPExcel.php');
+    require('./PHPExcel/PHPExcel/IOFactory.php');
+    $doc_name=$_FILES['doc']['tmp_name'];
+    $obj=PHPExcel_IOFactory::load($doc_name);
+    foreach($obj->getWorksheetIterator() as $all_data){
+        // all data is the associtive array in the all the data of the excel file
+       $number_of_row=$all_data->getHighestRow();
+       for($i=2; $i<=$number_of_row; $i++){
+        // here 0 and 1 is the number of index or colunmn in excel file 
+        // $i is th number of data in rows 
+echo $name=$all_data->getCellByColumnAndRow(0,$i)->getValue(); 
+echo $email=$all_data->getCellByColumnAndRow(1,$i)->getValue();
+echo "<br>";
+// here can write the sql query for inserting the data in database
+
+       }
+    }
+
+}
+else{
+    echo "format not mathed";
+}
+
 }
 ?>
 <form action="" method="POST" enctype="multipart/form-data">
